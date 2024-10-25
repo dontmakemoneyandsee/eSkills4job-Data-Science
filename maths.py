9eae7ff92685b82444e039323c34f4b1c6e85828
dontmakemoneyandsee <mandeyahmoari@gmail.com>
2024-10-25 15:08:27 +0100

    first commit

---
+++ maths.py
@@ -0,0 +1,16 @@
+## importing math class
+import random
+#import math
+#print(math.sqrt(25))
+#print(math.log(10))
+
+#pick_an_item = random.choice(["red ", " green ", " gray "])
+#print(pick_an_item)
+
+# generatea random itn number
+#rand_number = random.randint(0,20)
+#print(rand_number)
+
+##  generate a random number
+num =random.random()
+print(num)
---
+++ operators.py
@@ -0,0 +1,84 @@
+# print("enter[yes] or [no] to the following questions")
+# user_input_1=input("Is it raining: ")
+
+# if (user_input_1 == "yes"):
+#
+#     user_input_2 =input("Is it also windy?: ")
+#     if (user_input_2 == "yes"):
+#         print("It is too windy for an umbrella")
+#     else:
+#         print("take an umbrella")
+# else:
+#     print(" enjoy your day ")
+
+# print( "enter your age b/n 16 to 18 " )
+# user_input =int (input( "enter your age" ))
+# if ( user_input >= 18 ):
+#     print( "you can vote" )
+# if ( user_input == 17 ):
+#     print( "you can learn to drive" )
+# if (user_input== 16):
+#     print( " you can buy a lottery ticket" )
+# elif (user_input < 16 ):
+#     print( "you can go trick and treating "
+ #generate seq of nums b/n 1 and 11
+  #generate seq of nums b/n 1 and 11
+# for each_number in range(1, 11):
+#      print("each number: =< ", each_number )
+    
+     #generate seq of even nums b/n 1 and 11
+for each_number in range(2, 11, 2):
+     print("each number: =< ", each_number )
+     
+      #generate seq of nums b/n 1 and 11
+# for each_number in range(10, 1, -1):
+#      print("each number: =< ", each_number )
+    
+    #group of items = "riri"
+class_reg  = [ 'nat', 'lyd','mos','ross']
+for each_item in class_reg:
+    print(each_item)
+
+# user_name =input("enter your name: ")
+# for each_name in range (1, 4):
+#     print(" your name is ", user_name)
+    
+   #ask user to enter their name and a number 
+# user_name =input("enter your name: ")
+# number_times =int(input(" enter your number: "))
+# for each_name in range (1, number_times):
+#     print(" your name is ", user_name,)
+
+# print(" CLASS ATTENDANCE REGISTER")
+# attendance = input("have you registered or not ? ")
+# while (attendance == 'no'):
+#     username =input(" enter your name to register ")
+#     print("confirm name: ", username)
+#     ##loop control
+#     attendance =input(" have you registered or not ?")
+
+# user_input = 0
+# while(user_input <= 5 ): 
+#     user_input = int(input("enter a number:"))
+# print("the last number you entered was[", user_input, "]")
+
+action = " The boy is going to school " 
+print(action.upper())
+##
+#print(action.lower())
+
+#new_object =action.strip(" ")
+#print(new_object)
+
+
+ 
+
+    
+  
+  
+  
+    
+    
+
+       
+    
